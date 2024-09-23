b784097cbb76680a5ff136e4c3696f2e865e0f38
Faris <farissharayri@gmail.com>
2024-09-23 16:23:43 -0400

    implements model, builds main views ,some styling

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)  
+    quantity = models.PositiveIntegerField(default=1) 
+    purchased = models.BooleanField(default=False)
+    added_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f"{self.name} - {self.quantity}"
+
+    class Meta:
+        ordering = ['purchased', 'added_at']
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ def home(request):
 
 def about(request):
     return render(request,'about.html')
+
+def shopping_list(request):
+    return render(request, 'shopping-list.html')
