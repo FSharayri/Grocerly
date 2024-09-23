f5054809554ca5ecc1719c44b31f1746f40dd629
Faris <farissharayri@gmail.com>
2024-09-23 17:02:03 -0400

    minor fix on views

--- main_app/models.py
+++ main_app/models.py
@@ -8,9 +8,33 @@ class Item(models.Model):
     quantity = models.PositiveIntegerField(default=1) 
     purchased = models.BooleanField(default=False)
     added_at = models.DateTimeField(auto_now_add=True) 
+    category = models.CharField(max_length=100) # might change it later 
 
     def __str__(self):
         return f"{self.name} - {self.quantity}"
 
     class Meta:
         ordering = ['purchased', 'added_at']
+
+# grocery_categories = [
+#     "Fresh Produce",
+#     "Dairy",
+#     "Meat and Poultry",
+#     "Seafood",
+#     "Frozen Foods",
+#     "Bakery",
+#     "Beverages",
+#     "Canned and Packaged Goods",
+#     "Dry Goods & Grains",
+#     "Snacks",
+#     "Breakfast Items",
+#     "Condiments and Sauces",
+#     "Spices and Seasonings",
+#     "Baking and Cooking Ingredients",
+#     "Ethnic Foods",
+#     "Health & Wellness",
+#     "Household and Cleaning",
+#     "Baby & Child Care",
+#     "Personal Care",
+#     "Pet Care"
+# ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
 def home(request):
-    pass
+    return render(request,'home.html')
 
 def about(request):
     return render(request,'about.html')
 
+
 def shopping_list(request):
-    return render(request, 'shopping-list.html')
+    items = Item.objects.all()
+    #Cat.objects.filter(name='Rubber Biscuit')
+    #Cat.objects.filter(name__contains='Bis')
+    #Cat.objects.filter(age__lte=3)
+    #Cat.objects.get(id=1)
+    #Cat.objects.order_by('name')
+    return render(request, 'items/index.html', {'items':items})
